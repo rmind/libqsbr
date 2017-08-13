f147398b90d7082af791d847d0705cd00e251484
Mindaugas Rasiukevicius <rmind@noxt.eu>
2017-08-13 18:07:00 +0100

    G/C some unused helpers.

--- src/utils.h
+++ src/utils.h
@@ -42,89 +42,26 @@
 
 #include <stdbool.h>
 #include <inttypes.h>
-#include <limits.h>
 #include <assert.h>
-
-#ifndef __predict_true
-#define	__predict_true(x)	__builtin_expect((x) != 0, 1)
-#define	__predict_false(x)	__builtin_expect((x) != 0, 0)
-#endif
-
-#ifndef __constructor
-#define	__constructor		__attribute__((constructor))
-#endif
-
-#ifndef __packed
-#define	__packed		__attribute__((__packed__))
-#endif
-
-#ifndef __aligned
-#define	__aligned(x)		__attribute__((__aligned__(x)))
-#endif
-
-#ifndef __unused
-#define	__unused		__attribute__((__unused__))
-#endif
-
-#ifndef __arraycount
-#define	__arraycount(__x)	(sizeof(__x) / sizeof(__x[0]))
-#endif
-
-#ifndef __UNCONST
-#define	__UNCONST(a)		((void *)(unsigned long)(const void *)(a))
-#endif
-
-/*
- * Minimum, maximum and rounding macros.
- */
-
-#ifndef MIN
-#define	MIN(x, y)	((x) < (y) ? (x) : (y))
-#endif
-
-#ifndef MAX
-#define	MAX(x, y)	((x) > (y) ? (x) : (y))
-#endif
-
-#define	roundup(x, y)	((((x)+((y)-1))/(y))*(y))
-#define	rounddown(x,y)	(((x)/(y))*(y))
-
-#ifndef roundup2
-#define	roundup2(x, m)	(((x) + (m) - 1) & ~((m) - 1))
-#endif
-
-/*
- * Maths helpers: log2 on integer, fast division and remainder.
- */
-
-#ifndef flsl
-static inline int
-flsl(unsigned long x)
-{
-	return __predict_true(x) ?
-	    (sizeof(unsigned long) * CHAR_BIT) - __builtin_clzl(x) : 0;
-}
-#define	flsl(x)		flsl(x)
-#endif
-#ifndef flsll
-static inline int
-flsll(unsigned long long x)
-{
-	return __predict_true(x) ?
-	    (sizeof(unsigned long long) * CHAR_BIT) - __builtin_clzll(x) : 0;
-}
-#define	flsll(x)	flsl(x)
-#endif
-
-#ifndef ilog2
-#define	ilog2(x)	(flsl(x) - 1)
-#endif
 
 /*
  * A regular assert (debug/diagnostic only).
  */
 #if !defined(ASSERT)
 #define	ASSERT		assert
+#endif
+
+/*
+ * Branch prediction macros.
+ */
+
+#ifndef __predict_true
+#define	__predict_true(x)	__builtin_expect((x) != 0, 1)
+#define	__predict_false(x)	__builtin_expect((x) != 0, 0)
+#endif
+
+#ifndef __unused
+#define	__unused		__attribute__((__unused__))
 #endif
 
 /*
@@ -151,6 +88,7 @@ flsll(unsigned long long x)
 #define	atomic_compare_exchange_weak(ptr, expected, desired) \
     __sync_bool_compare_and_swap(ptr, expected, desired)
 #endif
+
 #ifndef atomic_exchange
 static inline void *
 atomic_exchange(volatile void *ptr, void *nptr)
@@ -165,43 +103,15 @@ atomic_exchange(volatile void *ptr, void *nptr)
 	return (void *)(uintptr_t)old; // workaround for gcc warnings
 }
 #endif
+
 #ifndef atomic_fetch_add
 #define	atomic_fetch_add(x,a)	__sync_fetch_and_add(x, a)
 #endif
 
 #ifndef atomic_thread_fence
-/*
- * memory_order_acquire	- membar_consumer/smp_rmb
- * memory_order_release	- membar_producer/smp_wmb
- */
-#define	memory_order_acquire	__atomic_thread_fence(__ATOMIC_ACQUIRE)
-#define	memory_order_release	__atomic_thread_fence(__ATOMIC_RELEASE)
-#define	atomic_thread_fence(m)	m
+#define	memory_order_acquire	__ATOMIC_ACQUIRE	// load barrier
+#define	memory_order_release	__ATOMIC_RELEASE	// store barrier
+#define	atomic_thread_fence(m)	__atomic_thread_fence(m)
 #endif
-
-/*
- * Exponential back-off for the spinning paths.
- */
-#define	SPINLOCK_BACKOFF_MIN	4
-#define	SPINLOCK_BACKOFF_MAX	128
-#if defined(__x86_64__) || defined(__i386__)
-#define SPINLOCK_BACKOFF_HOOK	__asm volatile("pause" ::: "memory")
-#else
-#define SPINLOCK_BACKOFF_HOOK
-#endif
-#define	SPINLOCK_BACKOFF(count)					\
-do {								\
-	int __i;						\
-	for (__i = (count); __i != 0; __i--) {			\
-		SPINLOCK_BACKOFF_HOOK;				\
-	}							\
-	if ((count) < SPINLOCK_BACKOFF_MAX)			\
-		(count) += (count);				\
-} while (/* CONSTCOND */ 0);
-
-/*
- * Cache line size - a reasonable upper bound.
- */
-#define	CACHE_LINE_SIZE		64
 
 #endif
